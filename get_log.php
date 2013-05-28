bc4b87a44b142801bf13276632a1b55418a307d3
Jeremy Bi <bixuanzju@gmail.com>
2013-05-28 17:52:26 +0800

    Rename file for consistency

--- get_log.php
+++ get_log.php
@@ -1,23 +1,54 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head> 
-	<title>get log</title> 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-</head>
+function die_with_error($error) {
+  $ret = array(
+	"status" => "Failed",
+	"error" => $error
+  );
+  die(json_encode($ret));
+}
 
-<body>
+$date = $_GET["date"];
 
-	<h1>提取日志</h1>
-    <p>选择您想要的日期</p>
-    
-    
-    <form action="query_action.php" method="get">
-  		<input name="chosen_date" type="date" />   
-		<input type ="submit"  value ="提 交">
-	</form>
+$hostname = 'localhost';
+$username = 'jeremy';
+$password = 'bbcc';
+$dbname = 'SE';
 
-</body>
+mysql_connect($hostname, $username, $password) or die_with_error(mysql_error());
+mysql_select_db($dbname) or die_with_error(mysql_error());
+mysql_set_charset('utf8');
 
-</html>
+
+$query = sprintf("SELECT * FROM payment WHERE finished_date = '%s';",
+				 mysql_real_escape_string($date));
+
+/* $query = "SELECT * FROM payment WHERE finished_date = '2013-5-23';"; */
+
+$result = mysql_query($query);
+
+if (! $result)
+  die_with_error(mysql_error());
+
+$result_array = array();
+while($row = mysql_fetch_assoc($result)){
+
+  array_push($result_array,
+             array(
+			   "order_id" => $row['order_id'],
+			   "buyer_id" => $row['buyer_id'],
+			   "seller_id" => $row['seller_id'],
+			   "status" => $row['status'],
+			   "total_amount" => $row['money_amount'],
+			   "trade_time" => $row['finished_date']
+			 ));
+
+}
+
+$ret = array(
+  "status" => "OK",
+  "data" => $result_array
+);
+die(json_encode($ret));
+
+?>
